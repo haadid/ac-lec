cbb7fd8c2c289ecab30d7f286ff1cb2ed5cac67e
eichdid <mrwn.hadid@std.unissula.ac.id>
2023-06-16 22:10:08 +0700

    second commit

--- app.py
+++ app.py
@@ -1,31 +1,19 @@
-import cv2
+from flask import Flask, request, jsonify, render_template
 import numpy as np
+import cv2
 import requests
-from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/detect', methods=['POST'])
-def detect():
-    if 'image' in request.files:
-        image = request.files['image']
-        img = cv2.imdecode(np.fromstring(image.read(), np.uint8), cv2.IMREAD_COLOR)
-        resized_img = cv2.resize(img, (224, 224))
-        preprocessed_img = np.expand_dims(resized_img, axis=0)
-        
-        url = 'http://localhost:8601/v1/models/acne:predict'
-        headers = {'Content-Type': 'application/json'}
-        data = {'instances': preprocessed_img.tolist()}
-        
-        response = requests.post(url, headers=headers, json=data)
-        
-        if response.status_code == 200:
-            predictions = response.json()['predictions']
-            num_acne = len(predictions)
-            acne_level = determine_acne_level(num_acne)
-            return jsonify({'num_acne': num_acne, 'acne_level': acne_level})
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-    return jsonify({'error': 'Invalid request'})
+def preprocess_image(image):
+    resized_image = cv2.resize(image, (224, 224))
+    normalized_image = resized_image / 255.0
+    preprocessed_image = np.expand_dims(normalized_image, axis=0)
+    return preprocessed_image
 
 def determine_acne_level(num_acne):
     if num_acne == 0:
@@ -36,6 +24,28 @@ def determine_acne_level(num_acne):
         return 'Severe'
     else:
         return 'Very Severe'
+
+@app.route('/detect', methods=['POST'])
+def detect():
+    image = request.files['image']
+    image_data = image.read()
+    nparr = np.frombuffer(image_data, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    preprocessed_image = preprocess_image(img)
+
+    url = 'http://localhost:8601/v1/models/acne:predict'
+    headers = {'Content-Type': 'application/json'}
+    data = {'instances': preprocessed_image.tolist()}
+
+    response = requests.post(url, headers=headers, json=data)
+
+    if response.status_code == 200:
+        predictions = response.json()['predictions']
+        num_acne = len(predictions)
+        acne_level = determine_acne_level(num_acne)
+        return jsonify({'num_acne': num_acne, 'acne_level': acne_level})
+
+    return jsonify({'error': 'Invalid request'})
 
 if __name__ == '__main__':
     app.run()
--- static/app.js
+++ static/app.js
@@ -1,60 +1,79 @@
 new Vue({
   el: '#app',
   data: {
-      numAcne: null,
-      acneLevel: null,
-      previewImage: ''
+    numAcne: null,
+    acneLevel: null,
+    previewImage: '',
+    showCamera: false
   },
   methods: {
-      handleFileUpload(event) {
-          const file = event.target.files[0];
-          this.previewImage = URL.createObjectURL(file);
-      },
-      upload() {
-          if (this.previewImage) {
-              const formData = new FormData();
-              formData.append('image', this.dataURLtoBlob(this.previewImage));
+    handleFileUpload(event) {
+      const file = event.target.files[0];
+      this.previewImage = URL.createObjectURL(file);
+    },
+    upload() {
+      if (this.previewImage) {
+        const formData = new FormData();
+        const blob = this.dataURLtoBlob(this.previewImage);
+        if (blob) {
+          formData.append('image', blob);
 
-              axios.post('/detect', formData, {
-                  headers: {
-                      'Content-Type': 'multipart/form-data'
-                  }
-              }).then(response => {
-                  this.numAcne = response.data.num_acne;
-                  this.acneLevel = response.data.acne_level;
-              }).catch(error => {
-                  console.error(error);
-              });
-          }
-      },
-      capture() {
-          const video = document.getElementById('video-player');
-          const canvas = document.createElement('canvas');
-          canvas.width = 300;
-          canvas.height = 300;
-          const context = canvas.getContext('2d');
-          context.drawImage(video, 0, 0, canvas.width, canvas.height);
-          this.previewImage = canvas.toDataURL('image/jpeg');
-      },
-      preview() {
-          const video = document.getElementById('video-player');
-          navigator.mediaDevices.getUserMedia({ video: true })
-              .then(stream => {
-                  video.srcObject = stream;
-              })
-              .catch(error => {
-                  console.error('Error accessing webcam: ', error);
-              });
-      },
-      dataURLtoBlob(dataURL) {
-          const byteString = atob(dataURL.split(',')[1]);
-          const mimeString = dataURL.split(',')[0].split(':')[1].split(';')[0];
-          const ab = new ArrayBuffer(byteString.length);
-          const ia = new Uint8Array(ab);
-          for (let i = 0; i < byteString.length; i++) {
-              ia[i] = byteString.charCodeAt(i);
-          }
-          return new Blob([ab], { type: mimeString });
+          axios.post('/detect', formData, {
+            headers: {
+              'Content-Type': 'multipart/form-data'
+            }
+          }).then(response => {
+            this.numAcne = response.data.num_acne;
+            this.acneLevel = response.data.acne_level;
+          }).catch(error => {
+            console.error(error);
+          });
+        }
       }
+    },
+    capture() {
+      const video = document.getElementById('video-player');
+      const canvas = document.createElement('canvas');
+      canvas.width = 300;
+      canvas.height = 300;
+      const context = canvas.getContext('2d');
+      context.drawImage(video, 0, 0, canvas.width, canvas.height);
+      this.previewImage = canvas.toDataURL('image/jpeg');
+    },
+    toggleCamera() {
+      const video = document.getElementById('video-player');
+      if (this.showCamera) {
+        // Matikan kamera
+        const stream = video.srcObject;
+        const tracks = stream.getTracks();
+        tracks.forEach(track => track.stop());
+        video.srcObject = null;
+      } else {
+        // Nyalakan kamera
+        navigator.mediaDevices.getUserMedia({ video: true })
+          .then(stream => {
+            video.srcObject = stream;
+          })
+          .catch(error => {
+            console.error('Error accessing webcam: ', error);
+          });
+      }
+      this.showCamera = !this.showCamera;
+    },
+    dataURLtoBlob(dataURL) {
+      try {
+        const byteString = atob(dataURL.split(',')[1]);
+        const mimeString = dataURL.split(',')[0].split(':')[1].split(';')[0];
+        const ab = new ArrayBuffer(byteString.length);
+        const ia = new Uint8Array(ab);
+        for (let i = 0; i < byteString.length; i++) {
+          ia[i] = byteString.charCodeAt(i);
+        }
+        return new Blob([ab], { type: mimeString });
+      } catch (error) {
+        console.error('Error converting data URL to blob: ', error);
+        return null;
+      }
+    }
   }
 });
